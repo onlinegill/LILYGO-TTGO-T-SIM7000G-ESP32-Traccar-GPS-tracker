8a8015854bac04ad51c0bc3a323c364613484b51
Sukhpal <1947686+onlinegill@users.noreply.github.com>
2024-01-16 19:12:17 -0600

    Update traccar.ino

--- traccar.ino
+++ traccar.ino
@@ -1,110 +1,135 @@
-#include <ArduinoHttpClient.h>
-#include <TinyGsmClient.h>
+#define SerialMon Serial
 
-const int UART_BAUD = 115200;
-const int PWR_PIN = 4;
-const int LED_PIN = 12;
-const int BAT_ADC = 35;
+// Set serial for AT commands (to the module)`
+// Use Hardware Serial on Mega, Leonardo, Micro
+#define SerialAT Serial1
 
-const uint32_t uS_TO_S_FACTOR = 1000000ULL;
-const int TIME_TO_SLEEP = 60;
+#define TINY_GSM_MODEM_SIM7000
+#define TINY_GSM_RX_BUFFER 1024  // Set RX buffer to 1Kb
 
+// See all AT commands, if wanted
+// #define DUMP_AT_COMMANDS
+String FINALLATI = "0", FINALLOGI = "0", FINALSPEED = "0", FINALALT = "0", FINALACCURACY = "0", FINALIGNITION = "false", ignition = "false";
+float battery = 0.0;
+// set GSM PIN, if any
+#define GSM_PIN ""
+
+// Your GPRS credentials, if any
 const char apn[] = "YOUR-APN";
 const char gprsUser[] = "";
 const char gprsPass[] = "";
-const char server[] = "Your Traccar Server IP";
-const int port = 5055;
-const String myid = "Traccar ID";
-const String GSM_PIN = "";
 
-TinyGsm modem(Serial1);
+const char server[] = "Your Traccar Server IP";
+
+const int port = 5055;
+String myid = "Traccar ID";
+
+#include <TinyGsmClient.h>
+#include <ArduinoHttpClient.h>
+
+
+#define DUMP_AT_COMMANDS
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
 TinyGsmClient client(modem);
 HttpClient http(client, server, port);
+
+#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP 60           // Time ESP32 will go to sleep (in seconds)
+
+#define UART_BAUD 115200
+#define PIN_DTR 25
+#define PIN_TX 27
+#define PIN_RX 26
+#define PWR_PIN 4
+#define SD_MISO 2
+#define SD_MOSI 15
+#define SD_SCLK 14
+#define SD_CS 13
+#define LED_PIN 12
+#define BAT_ADC 35
 
 float ReadBattery() {
   float vref = 1.100;
   uint16_t volt = analogRead(BAT_ADC);
-  return ((float)volt / 4095.0) * 2.0 * 3.3 * (vref);
+  float battery_voltage = ((float)volt / 4095.0) * 2.0 * 3.3 * (vref);
+  return battery_voltage;
 }
 
-void powerControl(int state) {
+void modemPowerOn() {
   pinMode(PWR_PIN, OUTPUT);
-  digitalWrite(PWR_PIN, state);
-  delay(state == HIGH ? 1000 : 1500);
+  digitalWrite(PWR_PIN, LOW);
+  delay(1000);  //Datasheet Ton mintues = 1S
+  digitalWrite(PWR_PIN, HIGH);
+}
+
+void modemPowerOff() {
+  pinMode(PWR_PIN, OUTPUT);
+  digitalWrite(PWR_PIN, LOW);
+  delay(1500);  //Datasheet Ton mintues = 1.2S
+  digitalWrite(PWR_PIN, HIGH);
 }
 
 void modemRestart() {
-  powerControl(LOW);
+  modemPowerOff();
   delay(1000);
-  powerControl(HIGH);
+  modemPowerOn();
 }
 
-void initializeModem() {
-  Serial.println("Initializing modem...");
-  if (!modem.restart()) {
-    Serial.println("Failed to restart modem, attempting to continue without restarting");
-  }
+void enableGPS(void) {
 
-  if (!GSM_PIN.isEmpty() && modem.getSimStatus() != 3) {
-    modem.simUnlock(GSM_PIN);
-  }
-}
-
-void connectToGPRS() {
-  Serial.print("Connecting to ");
-  Serial.print(apn);
-
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    Serial.println(" fail");
-    delay(30000);
-    return;
-  }
-
-  Serial.println(" success");
-}
-
-void enableGPS() {
-  Serial.println("Start positioning. Make sure to locate outdoors.");
+  Serial.println("Start positioning . Make sure to locate outdoors.");
   Serial.println("The blue indicator light flashes to indicate positioning.");
-
   modem.sendAT("+SGPIO=0,4,1,1");
   if (modem.waitResponse(10000L) != 1) {
-    Serial.println("SGPIO=0,4,1,1 false");
+    DBG(" SGPIO=0,4,1,1 false ");
   }
-
   modem.enableGPS();
 }
 
-void disableGPS() {
+void disableGPS(void) {
   modem.sendAT("+SGPIO=0,4,1,0");
   if (modem.waitResponse(10000L) != 1) {
-    Serial.println("SGPIO=0,4,1,0 false");
+    DBG(" SGPIO=0,4,1,0 false ");
   }
-
   modem.disableGPS();
 }
 
-void sendData(float lat, float lon, float speed, float alt, float accuracy, float battery) {
-  String params = String("/?id=") + myid +
-                  "&lat=" + String(lat, 8) +
-                  "&lon=" + String(lon, 8) +
-                  "&accuracy=" + String(accuracy, 2) +
-                  "&altitude=" + String(alt, 0) +
-                  "&speed=" + String(speed, 2) +
-                  "&battery=" + String(battery, 2);
+void send_data(float lat, float lon, float speed, float alt, float accuracy, float battery) {
 
+  String FINALLATI = "0", FINALLOGI = "0", FINALSPEED = "0", FINALALT = "0", FINALACCURACY = "0", FINALBAT = "0", FINALIGNITION = "false", FINALBATLEVEL = "";
+  FINALLATI = String(lat, 8);
+  FINALLOGI = String(lon, 8);
+  FINALSPEED = String(speed, 2);
+  FINALALT = String(alt, 0);
+  FINALACCURACY = String(accuracy, 2);
+  FINALIGNITION = String(ignition);
   if (battery == 0) {
-    params += "&ignition=true";
+    FINALBATLEVEL = "";
+    FINALBAT = "";
+    FINALIGNITION = "true";
   } else {
-    float batteryLevel = constrain(((battery - 3) / 1.2) * 100, 0, 100);
-    params += "&ignition=false&batteryLevel=" + String(batteryLevel, 0);
+    float batterylevel = (((float)battery - 3) / 1.2) * 100;
+    FINALBAT = String(battery, 2);
+    if (batterylevel > 100) {
+      batterylevel = 100;
+    }
+    FINALBATLEVEL = String(batterylevel, 0);
+    FINALIGNITION = "false";
   }
 
-  int err = http.post(params);
-  Serial.println(params);
 
+  int err = http.post("/?id=" + myid + "&lat=" + FINALLATI + "&lon=" + FINALLOGI + "&accuracy=" + FINALACCURACY + "&altitude=" + FINALALT + "&speed=" + FINALSPEED + "&battery=" + FINALBAT + "&ignition=" + FINALIGNITION + "&batteryLevel=" + FINALBATLEVEL);
+  Serial.println("/?id=" + myid + "&lat=" + FINALLATI + "&lon=" + FINALLOGI + "&accuracy=" + FINALACCURACY + "&altitude=" + FINALALT + "&speed=" + FINALSPEED + "&battery=" + FINALBAT + "&ignition=" + FINALIGNITION + "&batteryLevel=" + FINALBATLEVEL);
   if (err != 0) {
-    Serial.println("Failed to connect");
+    SerialMon.println(F("failed to connect"));
     delay(10000);
     return;
   }
@@ -117,50 +142,72 @@ void sendData(float lat, float lon, float speed, float alt, float accuracy, floa
   }
 
   String body = http.responseBody();
-  Serial.println("Response:");
-  Serial.println(body);
+  SerialMon.println(F("Response:"));
+  SerialMon.println(body);
 
+  // Shutdown
   http.stop();
-  Serial.println("Server disconnected. Will connect soon.");
+  SerialMon.println(F("Server disconnected bye bye will connect soon"));
 }
 
 void setup() {
-  Serial.begin(UART_BAUD);
+  // Set console baud rate
+  SerialMon.begin(115200);
+
+  delay(10);
+
+  // Set LED OFF
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
 
-  Serial1.begin(UART_BAUD, SERIAL_8N1, 26, 27);
+  SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
   delay(10000);
 
-  powerControl(HIGH);
-  initializeModem();
+  modemPowerOn();
+
+  Serial.println("Initializing modem...");
+  if (!modem.restart()) {
+    Serial.println("Failed to restart modem, attempting to continue without restarting");
+  }
+
+  // Unlock your SIM card with a PIN if needed
+  if (GSM_PIN && modem.getSimStatus() != 3) {
+    modem.simUnlock(GSM_PIN);
+  }
 }
 
 void loop() {
-  connectToGPRS();
+
+  // GPRS connection parameters are usually set after network registration
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    delay(30000);
+    return;
+  }
+  SerialMon.println(" success");
 
   if (modem.isGprsConnected()) {
-    Serial.println("GPRS connected");
+    SerialMon.println("GPRS connected");
   }
 
   enableGPS();
 
-  while (true) {
-    float battery = ReadBattery();
-    float lat, lon, speed, alt, accuracy;
-    int vsat, usat, year, month, day, hour, min, sec;
-
+  float lat, lon, speed, alt, accuracy;
+  int vsat, usat, year, month, day, hour, min, sec;
+  while (1) {
+    battery = ReadBattery();
     if (modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy, &year, &month, &day, &hour, &min, &sec)) {
-      sendData(lat, lon, speed, alt, accuracy, battery);
+      send_data(lat, lon, speed, alt, accuracy, battery);
     }
-
     digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-
     if (battery == 0) {
       delay(10000);
     } else {
       int count = 0;
-      while (battery > 0 && count < 90) {
+      while ((battery > 0) and (count < 90)) {
         battery = ReadBattery();
         delay(10000);
         count++;
